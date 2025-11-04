7f60e9d0404ce4d595934cfb160e911e0e62680d
Aleksander Sautycz <dziolex.21@gmail.com>
2025-11-04 09:52:45 +0100

    Kolorki

---
+++ lib/configs/color_data.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+abstract class AppColors {
+  static const purple1 = Color(0xFF9E72C3);
+  static const purple2 = Color(0xFF924DBF);
+  static const purple3 = Color(0xFF7338A0);
+  static const purple4 = Color(0xFF4A2574);
+  static const purple5 = Color(0xFF0F0529);
+  
+}
