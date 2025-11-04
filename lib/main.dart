4a393461cc7d91048c6219ce4ab1e6ef7cc76d74
Aleksander Sautycz <dziolex.21@gmail.com>
2025-11-04 12:24:46 +0100

    poprawki nazewnictwa

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tournament_app/configs/color_data.dart';
 import 'package:tournament_app/views/start_screen.dart';
-import 'package:tournament_app/views/tournament_lobby.dart';
-import 'package:tournament_app/views/voting_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Tournament App',
-      home: const TournamentScreen()
+      home: const StartScreen()
     );
   }
 }
